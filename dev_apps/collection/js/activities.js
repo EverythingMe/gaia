2e1e279e2bcce663426eb3142926cf3713bbfae8
Amir Nissim <amirnissim@gmail.com>
2014-05-11 18:36:57 +0300

    collections app: view collection

--- dev_apps/collection/js/activities.js
+++ dev_apps/collection/js/activities.js
@@ -4,29 +4,38 @@
 
   eme.init();
 
+  var elements = {
+    suggestions: document.getElementById('suggestions'),
+    results: document.getElementById('results')
+  }
+
   var Activities = {
     'create-collection': function(activity) {
-      var suggestions = document.querySelector('#suggestions');
 
       eme.api.Categories.list().then(function success(response) {
-
         var frag = document.createDocumentFragment();
-        response.response.categories.forEach(function (cat) {
+        response.response.categories.forEach(function each(suggestion) {
           var li = document.createElement('li');
-          li.textContent = cat.query + ' [' + cat.categoryId + ']';
-          frag.appendChild(li);
+
+          li.textContent = suggestion.query;
+          li.dataset.query = suggestion.query;
+          li.dataset.categoryId =suggestion.categoryId;
           li.addEventListener('click', select.bind(li));
+
+          frag.appendChild(li);
         });
 
-        suggestions.appendChild(frag);
+        elements.suggestions.appendChild(frag);
 
         function select() {
           this.removeEventListener('click', select);
 
           // Build and save a fake collection object
           var collection = {
+            // I think home2 should generate the id @kevingrandon
             id: Date.now() + '',
-            name: this.textContent
+            name: this.dataset.query,
+            categoryId: this.dataset.categoryId
           };
 
           CollectionsDatabase.add(collection).then(done, done);
@@ -52,6 +61,38 @@
     },
 
     'view-collection': function(activity) {
+      var categoryId = activity.source.categoryId;
+      var frag = document.createDocumentFragment();
+
+      eme.api.Apps.search({categoryId: categoryId}).then(
+        function success(response) {
+          response.response.apps.forEach(function each(app) {
+            var
+            li = document.createElement('li'),
+            img = document.createElement('img');
+
+            img.src = app.icon;
+
+            li.className = 'app-result';
+            li.textContent = app.name;
+            li.addEventListener('click', launch.bind(app));
+            li.appendChild(img);
+
+            frag.appendChild(li);
+          });
+
+          elements.results.appendChild(frag);
+        },
+        function error(){
+          alert('view-collection error');
+        }).catch(function fail() {
+          alert('view-collection fail');
+        });
+
+      function launch() {
+        window.open(this.appUrl);
+      }
+
       document.getElementById('close').addEventListener('click', function() {
         activity.postResult(true);
       });
